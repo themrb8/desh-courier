7d5af4035bbf25821db1394772e7478ea521b92a
themrb8 <officialroman786@gmail.com>
2023-01-23 19:22:39 +0600

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-guest-layout>
+
+</x-apguest-layout>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Helvitica Neue', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: '#5329FF',
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
